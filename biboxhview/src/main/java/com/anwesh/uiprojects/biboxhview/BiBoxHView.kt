3d14218e32f84d45893e9f5cab0fe72737618d47
Anwesh43 <anweshthecool0@gmail.com>
2019-05-10 11:44:11 +0530

    adding libraries and constants

--- biboxhview/src/main/java/com/anwesh/uiprojects/biboxhview/BiBoxHView.kt
+++ biboxhview/src/main/java/com/anwesh/uiprojects/biboxhview/BiBoxHView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.biboxhview
 /**
  * Created by anweshmishra on 10/05/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+
+val nodes : Int = 5
+val lines : Int = 2
+val parts : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
